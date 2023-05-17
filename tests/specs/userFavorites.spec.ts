f702cc425b1b66ea326c0d58f03860fe2b725d35
Karan Satia <karansatia@karans-mbp.lan>
2023-05-17 10:34:53 -0400

    resolving conflict from switching branches

--- tests/specs/userFavorites.spec.ts
+++ tests/specs/userFavorites.spec.ts
@@ -4,29 +4,26 @@ import { HomePage } from '../pages/HomePage.page';
 // throw in a beforeEach block to open the webpage
 test.beforeEach(async ({ homePage }) => homePage.open());
 
-test.describe('Logged in users should be able to see favorited listings', () => {
-    
+test.describe('authentication dependent tests', () => {
+
     test('user should be able to authenticate, create wishlists and delete wishlists', async ({ homePage }) => {
         await homePage.userIsLoggedIn()
-<<<<<<< Updated upstream
-    }),
-
-    test('user should be able to navigate to favorited listings via hamburger menu', async ({ homePage }) => {
-        await homePage.viewWishlists()
-    }),
-
-    test('user should be able to create wishlist from first homepage suggestion', async ({ homePage }) => {
-        await homePage.createWishlist('testWishlist')
-    }),
-
-    test('user should be able to delete wishlist from account wishlist page', async({ homePage }) => {
-        await homePage.deleteWishlist('testWishlist')
-=======
         await homePage.createWishlist('testWishlist')
         await homePage.deleteWishlist('testWishlist')
         await homePage.viewWishlists()
->>>>>>> Stashed changes
+    }),
+
+    // test block for checking payment on file. if there isn't one on file, run a test to find and add payment card 
+    test('user should be able to add payment methods to their account', async({homePage}) => {
+
     })
+
+
+    // test block for viewing user account review
+
+    // test block for adding dates and finding pricing
+
+    // find a way to test filters
 
 })
 
